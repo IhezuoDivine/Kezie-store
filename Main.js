bcaa896453622b41ff84206af2a3b2b834274b71
IhezuoDivine <ihezuodivine@gmail.com>
2025-05-14 08:46:39 +0100

    Add files via upload

--- Main.js
+++ Main.js
@@ -4,6 +4,8 @@
   const menubtn = document.getElementById("menu");
   const closebtn = document.getElementById("close");
   const category = document.getElementById("categorycontainer");
+  const viewmorecollection = document.getElementById("viewmorecollection");
+  const newcollectionimghide = document.getElementById("newcollectionimghide");
 
 
   menubtn.addEventListener("click",function(event){
@@ -11,16 +13,27 @@
     category.style.display="block";
     menubtn.style.display = 'none';
     closebtn.style.display="inline";
-    
   });
 
-  closeIcon.addEventListener('click', function (event) {
+  closebtn.addEventListener('click', function (event) {
     event.preventDefault();
     category.style.display = 'none';
     closebtn.style.display = 'none';
     menubtn.style.display = 'inline';
-
   });
 
+
+  viewmorecollection.addEventListener("click",function(event){
+    event.preventDefault();
+   
+    if (getComputedStyle(newcollectionimghide).display === "none") {
+      newcollectionimghide.style.display = "grid"; // or "block", depending on your layout
+      viewmorecollection.textContent="View Less"
+    }
+   else {
+      newcollectionimghide.style.display = "none";
+      viewmorecollection.textContent="View More Collection"
+    }
+  });
 
 });
